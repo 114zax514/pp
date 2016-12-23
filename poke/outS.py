f1d0d20e3a5fc26eea337ddf43130832b59829c8
114zax514 <xxzaxisxx0901@gmail.com>
2016-12-23 18:16:17 +0900

    1223

--- poke/outS.py
+++ poke/outS.py
@@ -1,2 +1,15 @@
 #coding: utf-8
+import urllib2
+from bs4 import BeautifulSoup
 
+html = urllib2.urlopen("http://pente.koro-pokemon.com/data/ranking-speed.shtml")
+soup = BeautifulSoup(html)
+th2 = soup(class_="th2")
+#  print th2
+#  print len(th2)
+for i in range(0,len(th2)):
+    print soup(class_="th2")[i].renderContents()
+
+poke = soup("*zukan*")
+for i in range(0,len(poke)):
+    print soup("*zukan*")[i].renderContents()
